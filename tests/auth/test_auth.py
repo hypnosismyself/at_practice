64334120c4651c434df9bc7220c6752f69646206
nikol <nikolskiy03@list.ru>
2026-07-01 18:52:36 +0300

    fix-test

--- tests/auth/test_auth.py
+++ tests/auth/test_auth.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 import allure
 import pytest
+import requests
 from selenium import webdriver
 from pages.login import LoginPage
 from helpers.config import Config
@@ -22,23 +23,24 @@ class LoginTest(TestCase):
         with allure.step('Открываем страницу авторизации'):
             self.login_page.open()
 
-    @allure.severity(allure.severity_level.CRITICAL)
     @pytest.mark.acceptance
     def test_01_basic_auth(self):
         """Авторизация по логину и паролю"""
-
         with allure.step('Открываем страницу авторизации'):
             self.login_page.open()
 
         with allure.step('Авторизуемся по логину и паролю'):
             self.login_page.auth_by_login_and_password(self.config.get('LOGIN'), self.config.get('PASS'))
 
-    @allure.severity(allure.severity_level.CRITICAL)
     @pytest.mark.smoke
     def test_02_fast_auth(self):
         """Авторизация по быстрой кнопке"""
         with allure.step('Авторизуемся кнопкой быстрой авторизации'):
             self.login_page.fast_auth(self.config.get('ROLE'))
+
+    def tearDown(self):
+        with allure.step('Чистим Local storage'):
+            self.driver.execute_script('localStorage.clear();')
 
     @classmethod
     def tearDownClass(cls):
